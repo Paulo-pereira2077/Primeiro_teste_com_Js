02f34f05efef7ef7ab9970c210fb119978822198
Paulo César Rocha <paulo.crrpereira@gmail.com>
2025-09-03 18:35:09 -0300

    Treinando os conceitos de criar e importar objetos no TypeScript

---
+++ ADO1/PauloCesarADO1.ts
@@ -0,0 +1,48 @@
+class Animal{
+
+    nome: String;
+    peso: number;
+    especie: String;
+    tempoCaca: number;
+
+    constructor (nome: String, especie: String){
+        this.nome = nome;
+        this.peso = 37;
+        this.especie = especie;
+    }
+
+    exibirEspecie(especie: String){
+        console.log(`ADO1: ${this.especie}`);
+    }
+
+    mergulhar(){
+         console.log(`O ${this.nome} mergulhou em baixo do gelo!`);
+    }
+
+    sair(){
+        console.log(`O ${this.nome} retornou à superfície!`);
+    }
+    
+    // saiu para caçar peixes
+    cacar(){
+        this.tempoCaca = 30;
+        console.log(`O ${this.nome} saiu para caçar peixes por ${this.tempoCaca}min.`)
+    }
+
+    engordar(peso: number){
+        this.peso += 10;
+        console.log(`O ${this.nome} pesava 37kg, porém comeu demais e chegou a pesar ${this.peso}kg!`)
+    }
+}
+
+const titulo = new Animal("Pinguim", "Pinguim-imperador");
+titulo.exibirEspecie("Pinguim");
+
+// pula uma linha
+console.log("");
+
+const ato1 = new Animal("Pinguim", "Pinguim-imperador");
+ato1.cacar();
+ato1.mergulhar();
+ato1.sair();
+ato1.engordar(10);
