e8f3fb256cfe1949dd7fc056de39641681c5cdac
Paulo César Rocha <paulo.crrpereira@gmail.com>
2025-09-03 18:58:03 -0300

    Atualizando o código para o JS moderno

--- ADO1/PauloCesarADO1.js
+++ ADO1/PauloCesarADO1.js
@@ -1,35 +1,41 @@
-var Animal = /** @class */ (function () {
-    function Animal(nome, especie) {
+class Animal {
+    constructor(nome, especie) {
         this.nome = nome;
         this.peso = 37;
         this.especie = especie;
     }
-    Animal.prototype.exibirEspecie = function (especie) {
-        console.log("ADO1: ".concat(this.especie));
-    };
-    Animal.prototype.mergulhar = function () {
-        console.log("O ".concat(this.nome, " mergulhou em baixo do gelo!"));
-    };
-    Animal.prototype.sair = function () {
-        console.log("O ".concat(this.nome, " retornou \u00E0 superf\u00EDcie!"));
-    };
-    // saiu para caçar peixes
-    Animal.prototype.cacar = function () {
+
+    exibirEspecie() {
+        console.log(`ADO1: ${this.especie}`);
+    }
+
+    mergulhar() {
+        console.log(`O ${this.nome} mergulhou em baixo do gelo!`);
+    }
+
+    sair() {
+        console.log(`O ${this.nome} retornou à superfície!`);
+    }
+
+    cacar() {
         this.tempoCaca = 30;
-        console.log("O ".concat(this.nome, " saiu para ca\u00E7ar peixes por ").concat(this.tempoCaca, "min."));
-    };
-    Animal.prototype.engordar = function (peso) {
+        console.log(`O ${this.nome} saiu para caçar peixes por ${this.tempoCaca}min.`);
+    }
+
+    engordar() {
         this.peso += 10;
-        console.log("O ".concat(this.nome, " pesava 37kg, por\u00E9m comeu demais e chegou a pesar ").concat(this.peso, "kg!"));
-    };
-    return Animal;
-}());
-var titulo = new Animal("Pinguim", "Pinguim-imperador");
-titulo.exibirEspecie("Pinguim");
+        console.log(`O ${this.nome} pesava 37kg, porém comeu demais e chegou a pesar ${this.peso}kg!`);
+    }
+}
+
+const titulo = new Animal("Pinguim", "Pinguim-imperador");
+titulo.exibirEspecie();
+
 // pula uma linha
 console.log("");
-var ato1 = new Animal("Pinguim", "Pinguim-imperador");
+
+const ato1 = new Animal("Pinguim", "Pinguim-imperador");
 ato1.cacar();
 ato1.mergulhar();
 ato1.sair();
-ato1.engordar(10);
+ato1.engordar();
